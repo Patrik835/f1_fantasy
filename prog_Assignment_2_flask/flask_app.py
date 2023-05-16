31a12f786cfb5c113b5d18698165819de52d8a8d
Patrik835 <22IMC10257@fh-krems.ac.at>
2023-05-16 22:38:21 +0200

    basic forms login base navbar designing

--- prog_Assignment_2_flask/flask_app.py
+++ prog_Assignment_2_flask/flask_app.py
@@ -1,16 +1,40 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, send_from_directory
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, AnyOf
 # Create an instance of Flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+
+app.config['SECRET_KEY'] = "Thisisasecretkey"
+
+class LoginForm(FlaskForm):
+    username = StringField("select your username", validators=[InputRequired(), Length(min=3, max=15)])
+    password = PasswordField("select your password", validators=[InputRequired(), Length(min=4, max=50)])
+    submit = SubmitField("Login")
+
 
 #create route that renders index.html template
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    username = None
+    password = None
+    form = LoginForm()
+    #validate form
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        form.username.data = ""
+        form.password.data = ""
+    return render_template("login.html", username = username, password = password, form = form)
+
+@app.route("/quiz_form")
+def quiz_form():
+    return render_template("quiz_form.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
