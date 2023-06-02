0f63d76f8db39a625257e7e3ad22e8c1cd48f5c5
Patrik835 <22IMC10257@fh-krems.ac.at>
2023-06-02 22:06:45 +0200

    deployment

--- prog_Assignment_2_flask/flask_app/__init__.py
+++ prog_Assignment_2_flask/flask_app/__init__.py
@@ -7,7 +7,9 @@ from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
 #add database
 #sqlite db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://luskdaihpcphpe:810b682365bcacc2ad7cf06fc6b5c1a5ec23554e06856c1cb697c75451921655@ec2-34-197-91-131.compute-1.amazonaws.com:5432/d3v3eph8tuc1hr'
+
 
 #secret key for forms
 app.config['SECRET_KEY'] = "Thisisasecretkey"
